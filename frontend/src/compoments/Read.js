db51e98314fc9e4026e0b510631bdee36a7513d6
Madush <it22281432@my.sliit.lk>
2024-07-10 19:02:30 +0530

    last

--- frontend/src/compoments/Read.js
+++ frontend/src/compoments/Read.js
@@ -1,76 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import '../style/table.css';
 
-
-
 function Read() {
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        function getStudents() {
+            axios.get("http://localhost:8070/student/")
+                .then((res) => {
+                    setStudents(res.data);
+                })
+                .catch((err) => {
+                    console.error(err.message);
+                    alert("Error fetching data: " + err.message);
+                });
+        }
+        getStudents();
+    }, []);
+
     return (
         <div>
             <h2>Responsive Table with RWD-Table-Patterns</h2>
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="table-responsive" data-pattern="priority-columns">
-                            <table summary="This table shows how to create responsive tables using RWD-Table-Patterns' functionality" class="table table-bordered table-hover">
-                                <caption class="text-center">An example of a responsive table based on RWD-Table-Patterns' <a href="http://gergeo.se/RWD-Table-Patterns/" target="_blank"> solution</a>:</caption>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12">
+                        <div className="table-responsive" data-pattern="priority-columns">
+                            <table summary="This table shows student data" className="table table-bordered table-hover">
+                                <caption className="text-center">Student Data Table</caption>
                                 <thead>
                                     <tr>
-                                        <th>Country</th>
-                                        <th data-priority="1">Languages</th>
-                                        <th data-priority="2">Population</th>
-                                        <th data-priority="3">Median Age</th>
-                                        <th data-priority="4">Area (Km²)</th>
+                                        <th>Name</th>
+                                        <th>Age</th>
+                                        <th>Gender</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>Argentina</td>
-                                        <td>Spanish (official), English, Italian, German, French</td>
-                                        <td>41,803,125</td>
-                                        <td>31.3</td>
-                                        <td>2,780,387</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Australia</td>
-                                        <td>English 79%, native and other languages</td>
-                                        <td>23,630,169</td>
-                                        <td>37.3</td>
-                                        <td>7,739,983</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Greece</td>
-                                        <td>Greek 99% (official), English, French</td>
-                                        <td>11,128,404</td>
-                                        <td>43.2</td>
-                                        <td>131,956</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Luxembourg</td>
-                                        <td>Luxermbourgish (national) French, German (both administrative)</td>
-                                        <td>536,761</td>
-                                        <td>39.1</td>
-                                        <td>2,586</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Russia</td>
-                                        <td>Russian, others</td>
-                                        <td>142,467,651</td>
-                                        <td>38.4</td>
-                                        <td>17,076,310</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Sweden</td>
-                                        <td>Swedish, small Sami- and Finnish-speaking minorities</td>
-                                        <td>9,631,261</td>
-                                        <td>41.1</td>
-                                        <td>449,954</td>
-                                    </tr>
+                                    {students.map((student, index) => (
+                                        <tr key={index}>
+                                            <td>{student.name}</td>
+                                            <td>{student.age}</td>
+                                            <td>{student.gender}</td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                                 <tfoot>
                                     <tr>
-                                        <td colspan="5" class="text-center">Data retrieved from <a href="http://www.infoplease.com/ipa/A0855611.html" target="_blank">infoplease</a> and <a href="http://www.worldometers.info/world-population/population-by-country/" target="_blank">worldometers</a>.</td>
+                                        <td colSpan="3" className="text-center">Data retrieved from MongoDB</td>
                                     </tr>
                                 </tfoot>
                             </table>
@@ -78,9 +54,8 @@ function Read() {
                     </div>
                 </div>
             </div>
-
-            <p class="p">Demo by George Martsoukos. <a href="http://www.sitepoint.com/responsive-data-tables-comprehensive-list-solutions" target="_blank">See article</a>.</p>
         </div>
-    )
+    );
 }
+
 export default Read;
