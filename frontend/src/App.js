2b52441de3735d2b3b8c673ed263eb95b0f6730c
Madush <it22281432@my.sliit.lk>
2024-07-20 00:04:21 +0530

    en

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './compoments/Header';
 import Footer from './compoments/Footer';
 import Regi from './compoments/Regi';
+import Update from './compoments/Update';
+import Read from './compoments/Read';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Header />
         <Routes>
         <Route path="/add" element={<Regi />} />
+        <Route path="/update" element={<Update />} />
+        <Route path="/read" element={<Read />} />
         </Routes>
        
         <Footer />
