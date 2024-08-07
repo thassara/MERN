56dc98b760de22cbac8b98e200e5b9ffe23e021c
Madusha Thassara <137921529+thassara@users.noreply.github.com>
2024-08-07 21:34:13 +0530

    new

--- frontend/src/pages/order/Order.js
+++ frontend/src/pages/order/Order.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from '../../compoments/Header'; // Adjusted path for Header
-import Footer from '../../compoments/Footer'; // Adjusted path for Footer
-import Add from '../../compoments/Order/AddForm';
+import Header from '../../compoments/Header '; // Adjusted path for Header
+import Footer from '../../compoments/Footer '; // Adjusted path for Footer
+import Add from '../../compoments/Order/AddForm ';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
