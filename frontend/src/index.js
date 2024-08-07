62cacfe7bbca767ffd18cd7bbe928bbc479767e8
Madusha Thassara <137921529+thassara@users.noreply.github.com>
2024-08-07 22:28:56 +0530

    a

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import Order from 'C:/Users/HP/Documents/GitHub/MERN/frontend/src/pages/order/Order';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
