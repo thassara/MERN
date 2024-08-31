c1e6b96af9d5f14d5ccc2ce5efc7ae90927f4596
JaninduMuthunayaka <it22072238@my.sliit.lk>
2024-08-31 06:06:39 +0530

    structure

--- frontend/src/pages/employee/EmployeeDashBoardPage.js
+++ frontend/src/pages/employee/EmployeeDashBoardPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import EmployeeDashBoardOne from '../../compoments/Employee/EmployeeDashBoardOne';
 
 function EmployeeDashBoardPage() {
   return (
     <div>
-      <h1>THis is the Employee dashboard Page</h1>
-      </div>
-       
+      <h1>This is the Employee Dashboard Page</h1>
+      <Routes>
+        <Route path="/" element={<EmployeeDashBoardOne />} />
+      </Routes>
+    </div>
   );
 }
+
 export default EmployeeDashBoardPage;
