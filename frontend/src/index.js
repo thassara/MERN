825bc3f3aadbf9f2f93457c3667335ee06f47e0a
JaninduMuthunayaka <it22072238@my.sliit.lk>
2024-08-31 06:31:18 +0530

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import EmployeeDashBoardPage from './pages/employee/EmployeeDashBoardPage';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <EmployeeDashBoardPage />
   </React.StrictMode>
   
 );
