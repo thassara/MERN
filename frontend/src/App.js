35f569c6d9703a8590097a04beebe193e8797da8
Janith Raajamunindu <it22234384@my.sliit.lk>
2024-10-10 14:29:45 +0530

    modified main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,122 +1,80 @@
+App.js
+
 import React from 'react';
 import './App.css';
-import Header from './compoments/Header';
-import Footer from './compoments/Footer';
-import Intro from './compoments/Intro';
-import CustomerLogin from './compoments/Login/CustomerLogin';
-import CustomerProfilePage from './pages/customer/CustomerProfilePage';
-import AdminLogin from './compoments/AdminLogin';
-import EmpLogin from './compoments/EmpLogin';
-import GMChoose from './compoments/GMChoose';
-import DMChoose from './compoments/DMChoose';
-import PMChoose from './compoments/PMChoose';
-import StockDashBoardPage from './pages/stock/StockDashBoard';
-import StockAddForm from './pages/stock/AddItems';
-import ContactSupplier from './pages/stock/contactSupplier'
-import AssignItems from './pages/stock/pendingOrders';
-import StockDetails from './pages/stock/stockDetails';
-import PackageDashBoardPage from './pages/package/PackageDashBoardPage';
-import OrderDashBoardPage from './pages/order/Or_Dashboard';
-import PaymentDashBoardPage from './pages/payment/PaymentDashBoardPage';
-import EmployeeDashBoardPage from './pages/employee/EmployeeDashBoardPage';
-import EmployeeProfile from './pages/employee/EmployeeProfile';
-import CustomerDashBoardPage from './pages/customer/CustomerDashBoardPage';
-import MachineDashBoardPage from './pages/machine/MachineDashBoardPage';
-import DeliveryDashBoardPage from './pages/delivery/DeliveryDashBoardPage';
-import Or_add from './compoments/Order/AddForm';
-import Or_Confirm from './compoments/Order/Or_confirm';
-// import Read from './components/Read';
-import MachineAdd from './compoments/Machine/machineadd';
-import MachineManager from './compoments/Machine/MachineManager';
-import UpdateMachine from './compoments/Machine/UpdateMachine';
-import MachineStatus from './compoments/Machine/MachineStatus';
-import AssignMachine from './compoments/Machine/AssignMachine';
-import Or_track from './pages/order/OrderTracking';
-import Or_update from './compoments/Order/Or_Updateform';
-import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
-//Employee Section
-import EditEmployee from './compoments/Employee/EditEmployee';
-import AddEmployee from './compoments/Employee/AddEmployee';
-import EditAttendance from './compoments/Employee/EditAttendance';
+import Header from './compoments/Header'; // Importing Header component
+import Footer from './compoments/Footer'; // Importing Footer component
+import Intro from './compoments/Intro'; // Importing Intro component
+import CustomerLogin from './compoments/Login/CustomerLogin'; // Importing Customer Login component
+import CustomerProfilePage from './pages/customer/CustomerProfilePage'; // Importing Customer Profile Page component
+import AdminLogin from './compoments/AdminLogin'; // Importing Admin Login component
+import EmpLogin from './compoments/EmpLogin'; // Importing Employee Login component
+import GMChoose from './compoments/GMChoose'; // Importing GM Choose component
+import DMChoose from './compoments/DMChoose'; // Importing DM Choose component
+import PMChoose from './compoments/PMChoose'; // Importing PM Choose component
+import StockDashBoardPage from './pages/stock/StockDashBoardPage'; // Importing Stock Dashboard Page component
+import PackageDashBoardPage from './pages/package/PackageDashBoardPage'; // Importing Package Dashboard Page component
+import OrderDashBoardPage from './pages/order/Or_Dashboard'; // Importing Order Dashboard Page component
+import PaymentDashBoardPage from './pages/payment/PaymentDashBoardPage'; // Importing Payment Dashboard Page component
+import EmployeeDashBoardPage from './pages/employee/EmployeeDashBoardPage'; // Importing Employee Dashboard Page component
+import CustomerDashBoardPage from './pages/customer/CustomerDashBoardPage'; // Importing Customer Dashboard Page component
+import MachineDashBoardPage from './pages/machine/MachineDashBoardPage'; // Importing Machine Dashboard Page component
+import DeliveryDashBoardPage from './pages/delivery/DeliveryDashBoardPage'; // Importing Delivery Dashboard Page component
+import Or_add from './compoments/Order/AddForm'; // Importing Order Add Form component
+import Or_Confirm from './compoments/Order/Or_confirm'; // Importing Order Confirm component
+import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom"; // Importing React Router components
 
+// Employee Section Imports
+import EditEmployee from './compoments/Employee/EditEmployee'; // Importing Edit Employee component
+import AddEmployee from './compoments/Employee/AddEmployee'; // Importing Add Employee component
 
-
-
-// import Regi from './components/Regi';
-// import Update from './components/Update';
-
+// Package Section Imports
+import CreatePackage from './compoments/Package/CreatePackage'; // Import Create Package component
+import PackageList from './compoments/Package/PackageList'; // Import Package List component
+import ReportGen from './pages/package/ReportGen'; // Import Package Report Generation component
 
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Intro />} />
-          <Route path="/*" element={<Outlet />} />
-          <Route path="/CustomerLogin" element={<CustomerLogin />} />
-          <Route path="/CustomerProfilePage" element={<CustomerProfilePage />} />
-          <Route path="/AdminLogin" element={<AdminLogin />} />
-          <Route path="/GMChoose" element={<GMChoose />} />
-          <Route path="/DMChoose" element={<DMChoose />} />
-          <Route path="/PMChoose" element={<PMChoose />} />
-          <Route path="/CustomerDashBoardPage" element={<CustomerDashBoardPage />} />
-          <Route path="/StockDashBoardPage" element={<StockDashBoardPage />} />
-          <Route path="/PackageDashBoardPage" element={<PackageDashBoardPage />} />
-          <Route path="/Or_add" element={<Or_add />} />
-          <Route path="/Or_Add/order-details" element={<Or_Confirm />} />
-          <Route path="/OrderDashBoardPage" element={<OrderDashBoardPage />} />
-          <Route path="/PaymentDashBoardPage" element={<PaymentDashBoardPage />} />
-          <Route path="/EmployeeDashBoardPage" element={<EmployeeDashBoardPage />} />
-          <Route path="/MachineDashBoardPage" element={<MachineDashBoardPage />} />
-          <Route path="/DeliveryDashBoardPage" element={<DeliveryDashBoardPage />} />
-      
-          <Route path="/MachineDashBoardPage/add-machine" element={<MachineAdd />} />
-          <Route path="/add-machine//MachineDashBoardPage" element={<MachineAdd />} />
-          <Route path="/machine-manager" element={<MachineManager />} />
-          <Route path="/update-machine/:id" element={<UpdateMachine />} />
-          <Route path="/MachineDashBoardPage/UpdateMachine" element={<UpdateMachine />} />
-          <Route path="/MachineDashBoardPage/Machine-Status" element={<MachineStatus />} />
-          <Route path="/MachineStatus/Assign-machine" element={<AssignMachine />} />
-        <Route path="/" element={<Intro/>} />
-        <Route path="/*" element={<Outlet/>}/>
-        <Route path="/CustomerLogin" element={<CustomerLogin/>}/> 
-        <Route path="/CustomerProfilePage" element={<CustomerProfilePage/>}/>
-        <Route path="/AdminLogin" element={<AdminLogin/>}/> 
-        <Route path="/EmpLogin" element={<EmpLogin/>}/> 
-        <Route path="/GMChoose" element={<GMChoose/>}/>
-        <Route path="/DMChoose" element={<DMChoose/>}/>  
-        <Route path="/PMChoose" element={<PMChoose/>}/> 
-        <Route path="/CustomerDashBoardPage" element={<CustomerDashBoardPage/>}/> 
-        <Route path="/StockDashBoardPage" element={<StockDashBoardPage/>}/> 
-        <Route path="/StockAddForm" element={<StockAddForm/>}/> 
-        <Route path="/ContactSupplier" element={<ContactSupplier/>}/>
-        <Route path="/AssignItems" element={<AssignItems/>}/>
-        <Route path="/StockDetails" element={<StockDetails/>}/>
-        <Route path="/PackageDashBoardPage" element={<PackageDashBoardPage/>}/>
-        <Route path="/Or_add" element={<Or_add/>}/>
-        <Route path="/OrderDashBoardPage/updateOrder/:id" element={<Or_update/>}/>
-        <Route path="/OrderDashBoardPage/orderTrack/:id" element={<Or_track />} />
-        <Route path="/Or_Add/order-details" element={<Or_Confirm/>}/>
-        <Route path="/OrderDashBoardPage" element={<OrderDashBoardPage/>}/> 
-        <Route path="/PaymentDashBoardPage" element={<PaymentDashBoardPage/>}/> 
-        <Route path="/EmployeeDashBoardPage" element={<EmployeeDashBoardPage/>}/> 
-
-        <Route path="/EmployeeProfile" element={<EmployeeProfile/>}/>   
-        <Route path="/MachineDashBoardPage" element={<MachineDashBoardPage/>}/> 
-        <Route path="/DeliveryDashBoardPage" element={<DeliveryDashBoardPage/>}/>  
+        <Header /> {/* Render Header component */}
         
-        /*EmployeeSection*/
-        <Route path="/EditEmployee/:empId" element={<EditEmployee />} />
-        <Route path="AddEmployee" element={<AddEmployee />} />
-        <Route path="/EditAttendance" element={<EditAttendance />} />
-   
+        <Routes>
+          {/* Define application routes */}
+          <Route path="/" element={<Intro/>} /> {/* Home page */}
+          <Route path="/*" element={<Outlet/>}/> {/* Outlet for nested routes */}
+          <Route path="/CustomerLogin" element={<CustomerLogin/>}/> {/* Customer Login */}
+          <Route path="/CustomerProfilePage" element={<CustomerProfilePage/>}/> {/* Customer Profile Page */}
+          <Route path="/AdminLogin" element={<AdminLogin/>}/> {/* Admin Login */}
+          <Route path="/EmpLogin" element={<EmpLogin/>}/> {/* Employee Login */}
+          <Route path="/GMChoose" element={<GMChoose/>}/> {/* GM Choose */}
+          <Route path="/DMChoose" element={<DMChoose/>}/> {/* DM Choose */}
+          <Route path="/PMChoose" element={<PMChoose/>}/> {/* PM Choose */}
+          <Route path="/CustomerDashBoardPage" element={<CustomerDashBoardPage/>}/> {/* Customer Dashboard */}
+          <Route path="/StockDashBoardPage" element={<StockDashBoardPage/>}/> {/* Stock Dashboard */}
+          <Route path="/PackageDashBoardPage" element={<PackageDashBoardPage/>}/> {/* Package Dashboard */}
+          <Route path="/Or_add" element={<Or_add/>}/> {/* Order Add Form */}
+          <Route path="/Or_Add/order-details" element={<Or_Confirm/>}/> {/* Order Confirmation */}
+
+          {/* Employee Section Routes */}
+          <Route path="/EditEmployee/:empId" element={<EditEmployee />} /> {/* Edit Employee */}
+          <Route path="/AddEmployee" element={<AddEmployee />} /> {/* Add Employee */}
+
+          {/* Package Section Routes */}
+          <Route path="/create" element={<CreatePackage />} /> {/* Create Package */}
+          <Route path="/packages" element={<PackageList />} /> {/* Display All Packages */}
+          <Route path="/package-dashboard" element={<PackageDashBoardPage />} />
+          <Route path="/reportGen" element={<ReportGen/>} />
+
+          
         </Routes>
-        <Footer />
+
+        <Footer /> {/* Render Footer component */}
       </div>
     </Router>
   );
 }
 
 export default App;
+
